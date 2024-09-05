5b0092f140cf7d9f0a2705496fe9c04932b7af03
Davi Lorena <davilorena2016@gmail.com>
2024-09-05 16:41:31 -0300

    currency identification

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
+// Moedas
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 // Adicionando o evento input que captura todos os dígitos "digitados" para manipular seus dados
 amount.addEventListener("input", () => {
@@ -7,3 +14,27 @@ const hasCaractersRegex = /\D+/g
 amount.value = amount.value.replace(hasCaractersRegex, "")
 })
 
+// Permitindo a seleção da moeda, captando os dados sem enviá-los capturando o submit do form e moldando-o
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    // Criado para a conversão do valor do real para tais moedas, especificando cada uma 
+switch (currency.value) {
+case "USD":
+    convertCurrency(amount.value, USD, "US$")
+break
+case "EUR": 
+convertCurrency(amount.value, EUR, "€")
+break
+case "GBP": 
+convertCurrency(amount.value, GBP, "£")
+break
+}
+
+
+}
+
+//função para converter a moeda 
+function convertCurrency(amount, price, symbol) {
+    console.log(amount, price, symbol)
+}
